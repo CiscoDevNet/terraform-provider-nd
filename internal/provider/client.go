64d5bf2e44ac9ae4a9b33462a0547965a66af5e4
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2024-08-06 20:00:32 +0530

    [ignore] Added retries support to the client

--- internal/provider/client.go
+++ internal/provider/client.go
@@ -9,6 +9,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
+	"math/rand"
+	"time"
 
 	"net/http"
 	"net/url"
@@ -18,12 +21,21 @@ import (
 	"github.com/Jeffail/gabs/v2"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golang.org/x/net/html"
 )
 
 const ndAuthPayload = `{
 	"userName": "%s",
 	"userPasswd": "%s"
 }`
+
+// Default timeout for NGINX in ND is 90 Seconds.
+// Allow the client to set a shorter or longer time depending on their
+// environment
+const DefaultReqTimeoutVal int = 100
+const DefaultBackoffMinDelay int = 4
+const DefaultBackoffMaxDelay int = 60
+const DefaultBackoffDelayFactor float64 = 3
 
 // Client is the main entry point
 type Client struct {
@@ -38,12 +50,16 @@ type Client struct {
 	proxyCreds         string
 	domain             string
 	skipLoggingPayload bool
+	maxRetries         int64
+	backoffMinDelay    int64
+	backoffMaxDelay    int64
+	backoffDelayFactor float64
 }
 
 // singleton implementation of a client
 var clientImpl *Client
 
-func initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain string, isInsecure bool) *Client {
+func initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain string, isInsecure bool, maxRetries int64) *Client {
 
 	bUrl, err := url.Parse(clientUrl)
 	if err != nil {
@@ -60,6 +76,7 @@ func initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain
 		proxyUrl:   proxyUrl,
 		proxyCreds: proxyCreds,
 		domain:     loginDomain,
+		maxRetries: maxRetries,
 	}
 
 	transport := &http.Transport{
@@ -90,9 +107,9 @@ func initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain
 }
 
 // GetClient returns a singleton
-func GetClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain string, isInsecure bool) *Client {
+func GetClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain string, isInsecure bool, maxRetries int64) *Client {
 	if clientImpl == nil {
-		return initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain, isInsecure)
+		return initClient(clientUrl, username, password, proxyUrl, proxyCreds, loginDomain, isInsecure, maxRetries)
 	}
 	return clientImpl
 }
@@ -210,50 +227,88 @@ func (c *Client) Do(req *http.Request, skipLoggingPayload bool) (*gabs.Container
 	log.Printf("[DEBUG] Beginning DO method %s", req.URL.String())
 	log.Printf("[TRACE] HTTP Request Method and URL: %s %s", req.Method, req.URL.String())
 
-	if !skipLoggingPayload {
-		log.Printf("[TRACE] HTTP Request Body: %v", req.Body)
+	var body []byte
+	if req.Body != nil && c.maxRetries != 0 {
+		body, _ = io.ReadAll(req.Body)
 	}
 
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if !skipLoggingPayload {
-		log.Printf("[TRACE] HTTP Response: %d %s %v", resp.StatusCode, resp.Status, resp)
-	} else {
-		log.Printf("[TRACE] HTTP Response: %d %s", resp.StatusCode, resp.Status)
-	}
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	bodyStr := string(bodyBytes)
-	err = resp.Body.Close()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if !skipLoggingPayload {
-		log.Printf("[DEBUG] HTTP response unique string %s %s %s", req.Method, req.URL.String(), bodyStr)
-	}
-
-	if req.Method != "DELETE" && resp.StatusCode != 204 {
-		obj, err := gabs.ParseJSON(bodyBytes)
-		if err != nil {
-			log.Printf("Error occurred while json parsing %+v", err)
-			return nil, resp, err
+	for attempts := int64(0); ; attempts++ {
+		if c.maxRetries != 0 {
+			req.Body = io.NopCloser(bytes.NewBuffer(body))
 		}
-		log.Printf("[DEBUG] Exit from do method")
-		return obj, resp, err
-	} else if req.Method == "DELETE" && resp.StatusCode == 204 {
-		return nil, resp, nil
-	} else if resp.StatusCode == 204 {
-		return nil, nil, nil
-	} else {
-		return nil, resp, err
+
+		if !skipLoggingPayload {
+			log.Printf("[TRACE] HTTP Request Body: %v", req.Body)
+		}
+
+		resp, err := c.httpClient.Do(req)
+
+		if err != nil {
+			if ok := c.backoff(attempts); !ok {
+				log.Printf("[ERROR] HTTP Connection error occured: %+v", err)
+				log.Printf("[DEBUG] Exit from Do method")
+				return nil, nil, errors.New(fmt.Sprintf("Failed to connect to ND. Verify that you are connecting to an ND.\nError message: %+v", err))
+			} else {
+				log.Printf("[ERROR] HTTP Connection failed: %s, retries: %v", err, attempts)
+				continue
+			}
+		}
+
+		if !skipLoggingPayload {
+			log.Printf("[TRACE] HTTP Response: %d %s %v", resp.StatusCode, resp.Status, resp)
+		} else {
+			log.Printf("[TRACE] HTTP Response: %d %s", resp.StatusCode, resp.Status)
+		}
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		bodyStr := string(bodyBytes)
+		err = resp.Body.Close()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if !skipLoggingPayload {
+			log.Printf("[DEBUG] HTTP response unique string %s %s %s", req.Method, req.URL.String(), bodyStr)
+		}
+
+		if req.Method != "DELETE" && resp.StatusCode != 204 {
+			obj, err := gabs.ParseJSON(bodyBytes)
+			if err != nil {
+				log.Printf("Error occurred while json parsing %+v", err)
+				return nil, resp, err
+			}
+			log.Printf("[DEBUG] Exit from do method")
+			return obj, resp, err
+		} else if req.Method == "DELETE" && resp.StatusCode == 204 {
+			log.Printf("[DEBUG] Exit from do method")
+			return nil, resp, nil
+		} else if resp.StatusCode == 204 {
+			log.Printf("[DEBUG] Exit from do method")
+			return nil, nil, nil
+		} else {
+			if ok := c.backoff(attempts); !ok {
+				obj, err := gabs.ParseJSON(bodyBytes)
+				if err != nil {
+					log.Printf("[ERROR] Error occured while json parsing: %+v with HTTP StatusCode 405, 500-504", err)
+
+					// If nginx is too busy or the page is not found, ND's nginx will response with an HTML doc instead of a JSON Response.
+					// In those cases, parse the HTML response for the message and return that to the user
+					htmlErr := c.checkHtmlResp(bodyStr)
+					log.Printf("[ERROR] Error occured while json parsing: %s", htmlErr.Error())
+					log.Printf("[DEBUG] Exit from Do method")
+					return nil, resp, errors.New(fmt.Sprintf("Failed to parse JSON response from: %s. Verify that you are connecting to an ND.\nHTTP response status: %s\nMessage: %s", req.URL.String(), resp.Status, htmlErr))
+				}
+				log.Printf("[DEBUG] Exit from Do method")
+				return obj, resp, nil
+			} else {
+				log.Printf("[ERROR] HTTP Request failed: StatusCode %v, Retries: %v", resp.StatusCode, attempts)
+				continue
+			}
+		}
 	}
 }
 
@@ -297,4 +352,103 @@ func DoRestRequest(ctx context.Context, diags *diag.Diagnostics, client *Client,
 	}
 
 	return cont
+}
+
+func (c *Client) backoff(attempts int64) bool {
+	log.Printf("[DEBUG] Begining backoff method: attempts %v on %v", attempts, c.maxRetries)
+	if attempts >= c.maxRetries {
+		log.Printf("[DEBUG] Exit from backoff method with return value false")
+		return false
+	}
+
+	minDelay := time.Duration(DefaultBackoffMinDelay) * time.Second
+	if c.backoffMinDelay != 0 {
+		minDelay = time.Duration(c.backoffMinDelay) * time.Second
+	}
+
+	maxDelay := time.Duration(DefaultBackoffMaxDelay) * time.Second
+	if c.backoffMaxDelay != 0 {
+		maxDelay = time.Duration(c.backoffMaxDelay) * time.Second
+	}
+
+	factor := DefaultBackoffDelayFactor
+	if c.backoffDelayFactor != 0 {
+		factor = c.backoffDelayFactor
+	}
+
+	min := float64(minDelay)
+	backoff := min * math.Pow(factor, float64(attempts))
+	if backoff > float64(maxDelay) {
+		backoff = float64(maxDelay)
+	}
+	backoff = (rand.Float64()/2+0.5)*(backoff-min) + min
+	backoffDuration := time.Duration(backoff)
+	log.Printf("[TRACE] Starting sleeping for %v", backoffDuration.Round(time.Second))
+	time.Sleep(backoffDuration)
+	log.Printf("[DEBUG] Exit from backoff method with return value true")
+	return true
+}
+
+// If nginx is too busy or the page is not found, ND's nginx will response with an HTML doc instead of a JSON Response.
+// In those cases, parse the HTML response for the message and return that to the user
+//
+// Sample Response Body: https://github.com/nginx/nginx-releases/blob/master/html/50x.html
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <title>Error</title>
+// <style>
+//
+//	body {
+//	    width: 35em;
+//	    margin: 0 auto;
+//	    font-family: Tahoma, Verdana, Arial, sans-serif;
+//	}
+//
+// </style>
+// </head>
+// <body>
+// <h1>An error occurred.</h1>
+// <p>Sorry, the page you are looking for is currently unavailable.<br/>
+// Please try again later.</p>
+// <p>If you are the system administrator of this resource then you should check
+// the <a href="http://nginx.org/r/error_log">error log</a> for details.</p>
+// <p><em>Faithfully yours, nginx.</em></p>
+// </body>
+// </html>
+//
+// Sample return error:
+// An error occurred. Sorry, the page you are looking for is currently unavailable. If you are the system administrator of this
+// resource then you should check the error log for details. Faithfully yours, nginx.
+func (c *Client) checkHtmlResp(body string) error {
+	reader := strings.NewReader(body)
+	tokenizer := html.NewTokenizer(reader)
+	errStr := ""
+	prevTag := ""
+	for {
+		tt := tokenizer.Next()
+		if tt == html.ErrorToken {
+			break
+		}
+		tag, _ := tokenizer.TagName()
+		token := tokenizer.Token()
+
+		if prevTag == "a" || prevTag == "p" || prevTag == "body" {
+			data := strings.TrimSpace(token.Data)
+			if data == "" {
+				continue
+			}
+			if errStr == "" {
+				errStr = data
+			} else {
+				errStr = errStr + " " + data
+			}
+		}
+		prevTag = string(tag)
+	}
+	if errStr == "" {
+		errStr = "Empty ND HTML Response"
+	}
+	log.Printf("[DEBUG] HTML Error Parsing Result: %s", errStr)
+	return fmt.Errorf(errStr)
 }
