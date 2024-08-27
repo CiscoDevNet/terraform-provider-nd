sajagana/terraform-provider-nd1

[minor_change] Added nd_site and nd_version resource and data-sources

examples/data-sources/nd_site/main.tf
@@ -1,0 +1,3 @@
+data "nd_site" "example_site" {
+  site_name = "example_site"
+}

vendor/github.com/ciscoecosystem/mso-go-client/client/auth.go
@@ -1,0 +1,45 @@
+package client
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Auth struct {
+	Token  string
+	Expiry time.Time
+}
+
+func (au *Auth) IsValid() bool {
+	if au.Token != "" && au.Expiry.Unix() > au.estimateExpireTime() {
+		return true
+	}
+	return false
+}
+
+func (t *Auth) CalculateExpiry(willExpire int64) {
+	t.Expiry = time.Unix((time.Now().Unix() + willExpire), 0)
+}
+
+func (t *Auth) estimateExpireTime() int64 {
+	return time.Now().Unix() + 3
+}
+
+func (client *Client) InjectAuthenticationHeader(req *http.Request, path string) (*http.Request, error) {
+	log.Printf("[DEBUG] Begin Injection")
+	if client.AuthToken == nil || !client.AuthToken.IsValid() {
+
+		err := client.Authenticate()
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", client.AuthToken.Token))
+	req.Header.Set("Cookie", fmt.Sprintf("AuthCookie=%s", client.AuthToken.Token))
+	return req, nil
+}

CiscoDevNet/terraform-provider-nd

examples/data-sources/nd_site/main.tf
@@ -1,0 +1,3 @@
+data "nd_site" "example" {
+  site_name = "example"
+}

internal/provider/auth.go
@@ -1,0 +1,44 @@
+package provider
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Auth struct {
+	Token  string
+	Expiry time.Time
+}
+
+func (au *Auth) IsValid() bool {
+	if au.Token != "" && au.Expiry.Unix() > au.estimateExpireTime() {
+		return true
+	}
+	return false
+}
+
+func (t *Auth) CalculateExpiry(willExpire int64) {
+	t.Expiry = time.Unix((time.Now().Unix() + willExpire), 0)
+}
+
+func (t *Auth) estimateExpireTime() int64 {
+	return time.Now().Unix() + 3
+}
+
+func (client *Client) InjectAuthenticationHeader(req *http.Request, path string) (*http.Request, error) {
+	log.Printf("[DEBUG] Begin Injection")
+	if client.AuthToken == nil || !client.AuthToken.IsValid() {
+		err := client.Authenticate()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", client.AuthToken.Token))
+	req.Header.Set("Cookie", fmt.Sprintf("AuthCookie=%s", client.AuthToken.Token))
+
+	return req, nil
+}